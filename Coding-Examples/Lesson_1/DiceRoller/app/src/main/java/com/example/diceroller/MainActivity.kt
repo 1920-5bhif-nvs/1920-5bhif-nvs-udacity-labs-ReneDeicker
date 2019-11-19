3b3929c574d3c96e2c92dd4c28d3e3dbb40a9603
ReneDeicker <rene.deicker1105@gmail.com>
2019-11-19 18:21:33 +0100

    Lesson 1 Final Code

--- Coding-Examples/Lesson_1/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ Coding-Examples/Lesson_1/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,10 +18,11 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
             rollDice()
         }
+
+        diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
-        val diceImage: ImageView = findViewById(R.id.dice_image)
         val randomInt = Random().nextInt(6) + 1
 
         val drawableResource = when (randomInt) {
